1804a1eced8b900a0dc9388d7badb8d6cfdda8f0
Ilianx <diegomxxx@hotmail.com>
2021-10-26 02:20:54 -0500

    fixing lints

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const morgan = require('morgan');
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const router = require('./src/routes');
 
